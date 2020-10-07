0fb16921125de473d7bfbc50d5e5d370b326e92c
Brett Karr <b.karr@polyclip.us>
2020-10-06 21:50:38 -0500

    stack.ls8 is running correctly

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,10 +4,13 @@
 import sys
 
 # from print8.ls8:
-LDI = 0b10000010    # LOAD
-PRN = 0b01000111    # PRINT
-HLT = 0b00000001    # HALT
-MUL = 0b10100010    # MULTIPLY
+LOAD    = 0b10000010    # LOAD
+PRINT   = 0b01000111    # PRINT
+HALT    = 0b00000001    # HALT
+MULT    = 0b10100010    # MULTIPLY NUMBERS TOGETHER
+PUSH    = 0b01000101    # PUSH ONTO STACK
+POP     = 0b01000110    # POP OFF THE STACK
+ADD     = 0b10100110    # ADD NUMBERS TOGETHER
 
 
 class CPU:
@@ -15,9 +18,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256  # 256 bytes of RAM
-        self.registers = [0] * 8  #
-        self.pc = 0  # program counter
+        self.ram = [0] * 256  # 256 bytes of RAM/memory
+        self.registers = [0] * 8  # registers 0:7 are saved for quick access
+        self.registers[7] = 0xF4  # 244
+        self.pc = 0  # program counter/pointer
 
     def load(self):
         """Load a program into memory."""
@@ -26,12 +30,12 @@ class CPU:
         # For now, we've just hardcoded a program:
         # program = [
         #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
+        #     0b10000010, # LOAD R0,8
         #     0b00000000,
         #     0b00001000,
-        #     0b01000111, # PRN R0
+        #     0b01000111, # PRINT R0
         #     0b00000000,
-        #     0b00000001, # HLT
+        #     0b00000001, # HALT
         # ]
         #
         # for instruction in program:
@@ -39,28 +43,29 @@ class CPU:
         #     address += 1
 
         if len(sys.argv) != 2:
-            print("Usage: ls8.py filename")
+            print("Usage: py ls8.py <filename.py>")
             sys.exit(1)
 
+        address = 0
         try:
-            address = 0
             with open(sys.argv[1]) as f:
-                # print('sys: ', sys.argv)  # sys.argv = ['ls8.py', 'examples/<filename>']
+                # print(sys.argv)  # sys.argv = ['ls8.py', 'examples/<filename>']
                 for line in f:
                     # separate each line into the binary code from the file
-                    code_val = line.split('#')[0].strip()   # ex: 100000010
+                    code_number = line.split('#')[0].strip()   # ex: 100000010
+                    code_number = line[:line.find('#')]
 
-                    if code_val == "":
-                        continue
+                    if code_number == "":
+                        continue  # skips to the next iteration
 
                     try:
-                        code_val = int(code_val, 2)
+                        code_number = int(code_number, 2)
 
                     except ValueError:
-                        print(f"Invalid Number: {code_val}")
+                        print(f"Invalid Number: {code_number}")
                         sys.exit(1)
 
-                    self.ram_write(address, code_val)
+                    self.ram_write(address, code_number)
                     address += 1
 
         except FileNotFoundError:
@@ -74,7 +79,7 @@ class CPU:
         if op == "ADD":
             self.registers[reg_a] += self.registers[reg_b]
         #elif op == "SUB": etc
-        elif op == "MUL":
+        elif op == "MULT":
             self.registers[reg_a] *= self.registers[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
@@ -113,37 +118,86 @@ class CPU:
     def run(self):
         """Run the CPU."""
         running = True
-        print('running program...')
 
         while running:
             # Instruction Register (IR) is where we store the result from ram_read
-            IR = self.ram_read(self.pc)
+            IR = self.ram_read(self.pc)  # should be the "command"/instructions
 
-            # read and save the bytes from the next 2 RAM locations after pc location.
-            # *pc is the 'starting' point/signal, and the next 2 are the data we want to utilize
-            op_1 = self.ram_read(self.pc + 1)
-            op_2 = self.ram_read(self.pc + 2)
+            num_args = IR >> 6  # returns 2 for commands>2 digits, 1 for commands>1 digit, 0 for commands==1 digit
 
-            if IR == HLT:  # HALT/exit loop
+            # print('IR: ', IR)
+
+            if IR == HALT:  # HALT/exit loop
                 running = False
-                self.pc += 1
-
-            elif IR == PRN:  # PRINT the register
-                print(self.registers[op_1])
                 # self.pc += 1
+
+            elif IR == PRINT:  # PRINT the register
+                reg_idx = self.ram_read(self.pc + 1)
+                print(self.registers[reg_idx])
+                self.pc += 1
+                # self.pc += 2
+
+            elif IR == LOAD:  # LOAD
+                # get the register index (pc + 1) and the value (pc + 2)
+                reg_idx = self.ram_read(self.pc + 1)
+                value = self.ram_read(self.pc + 2)
+
+                # save the value into the correct register
+                self.registers[reg_idx] = value
+                self.pc += 2
+                # self.pc += 3
+
+            elif IR == ADD:
+                # pull out arguments
+                reg_idx_1 = self.ram_read(self.pc + 1)
+                reg_idx_2 = self.ram_read(self.pc + 2)
+
+                # add register 2 to register 1
+                self.registers[reg_idx_1] = self.registers[reg_idx_1] + self.registers[reg_idx_2]
                 self.pc += 2
 
-            elif IR == LDI:  # LOAD
-                self.registers[op_1] = op_2
-                # self.pc += 2
-                self.pc += 3
+            elif IR == MULT:
+                # pull out arguments
+                reg_idx_1 = self.ram_read(self.pc + 1)
+                reg_idx_2 = self.ram_read(self.pc + 2)
 
-            elif IR == MUL:
-                self.registers[op_1] *= self.registers[op_2]
-                # self.pc += 2
-                self.pc += 3
+                self.registers[reg_idx_1] *= self.registers[reg_idx_2]
+                self.pc += 2
+                # self.pc += 3
+                # print('7: ', self.registers[7])
+
+            elif IR == PUSH:
+                # **SP = Stack Pointer
+                # 1. Decrement the 'SP'
+                self.registers[7] -= 1  # decrement the 'SP' by 1
+
+                # 2. Copy the value in the given register to the address pointed to by "SP"
+                reg_idx = self.ram_read(self.pc + 1)
+                value = self.registers[reg_idx]
+
+                # 3. SP = registers[7]
+                # memory[SP] = value
+                SP = self.registers[7]
+                self.ram_write(SP, value)
+
+                self.pc += 1
+
+            elif IR == POP:
+                # 1. Copy the value from the address pointed to by "SP" to given register
+                # address of SP, its value at the address, and the register idx
+                SP = self.registers[7]
+                value = self.ram_read(SP)
+                reg_idx = self.ram_read(self.pc + 1)
+
+                # place value into the register idx
+                self.registers[reg_idx] = value
+
+                self.registers[7] += 1
+
+                self.pc += 1
 
             else:
+                print('else: ', IR)
                 sys.exit()
 
-            # self.pc += 1
+            self.pc += 1
